5e85f2b29dde2d04caeefcb4b2d19196e445030b
José LOPEZ <jose.rafael89@gmail.com>
2019-05-10 10:05:32 +0200

    Improve numeric numpy array conversion

--- +pythontools/py2matlab.m
+++ +pythontools/py2matlab.m
@@ -1,6 +1,14 @@
 function outObject = py2matlab(inObject)
 %PY2MATLAB Summary of this function goes here
 %   Detailed explanation goes here
+numericTypes = {...
+            'py.int', 'py.float', ...
+            'py.numpy.float', 'py.numpy.float16', 'py.numpy.float32', ...
+            'py.numpy.float64', 'py.numpy.int', 'py.numpy.int0', ...
+            'py.numpy.int8', 'py.numpy.int16', 'py.numpy.int32', ...
+            'py.numpy.int64', 'py.numpy.uint', 'py.numpy.uint0', ...
+            'py.numpy.uint8', 'py.numpy.uint16', 'py.numpy.uint32', ...
+            'py.numpy.uint64', 'py.numpy.uintc', 'py.numpy.uintp'};
 switch class(inObject)
     case 'py.dict'
         % Convert to struct
@@ -10,20 +18,31 @@ switch class(inObject)
         end 
     case {'py.list', 'py.tuple'}
         % Convert to cell array
-        outObject = cellfun(@pythontools.py2matlab, cell(inObject));
+        outObject = cellfun(@pythontools.py2matlab, cell(inObject), ...
+            'UniformOutput', false);
     case  'py.numpy.ndarray'
         % Convert to py.list first then to cell array
         % Reshape to 1-N
         outObject = inObject.reshape(inObject.size);
         % Transform to matlab object
-        outObject = pythontools.py2matlab(outObject.tolist());
+        switch ['py.numpy.' char(py.str(inObject.dtype))]
+            case numericTypes
+                % https://fr.mathworks.com/matlabcentral/answers/157347-convert-python-numpy-array-to-double
+                outObject = double(py.array.array('d', py.numpy.nditer(inObject)));
+            otherwise
+                outObject = pythontools.py2matlab(outObject.tolist());         
+        end        
         % Reshape into original size
-        originalSize = cell(inObject.shape);
-        outObject = reshape(outObject, originalSize{:});
-    case {...
-            'py.int', 'py.float', ...
-            'py.numpy.float', 'py.numpy.float16', 'py.numpy.float32', 'py.numpy.float64', ...
-            'py.numpy.int', 'py.numpy.int0', 'py.numpy.int8', 'py.numpy.int16', 'py.numpy.int32', 'py.numpy.int64'}
+        if py.len(inObject.shape) == 1
+            originalSize = {1, double(inObject.shape{1})};
+        else
+            originalSize = pythontools.py2matlab(inObject.shape);
+        end
+        
+        if ~isempty(originalSize)
+            outObject = reshape(outObject, originalSize{:});
+        end
+    case numericTypes
         % Convert to double
         outObject = double(py.float(inObject));
     case 'py.pandas.core.frame.DataFrame'
@@ -45,5 +64,3 @@ switch class(inObject)
         % Not necessary to convert or not yet supported
         outObject = inObject;
 end
-
-
